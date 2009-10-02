6ef0a1c164e85f76fca242e35124dbcbb3cf2ae3
Edward Benson <=>
2009-10-02 15:33:31 -0400

    removed tmp

--- tmp/Django-1.1/build/scripts-2.6/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Library/Frameworks/Python.framework/Versions/2.6/Resources/Python.app/Contents/MacOS/Python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
