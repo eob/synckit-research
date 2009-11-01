dd6b2bcd19775b940da5c45fdb2f806786dbe889
Adam Marcus <marcua@marcua.net>
2009-11-01 12:48:30 -0500

    wiki parameters

--- perf/usage_generator.py
+++ perf/usage_generator.py
@@ -29,8 +29,8 @@ if WIKI_TEST:
 #    NUM_USERS  = 20
 #    PERCENT_NEW = 0.3
     FROM_DATE = datetime.datetime(2010, 05, 01)
-    TO_DATE = datetime.datetime(2010, 05, 07)
-    SITE = Wiki("", 0.3)
+    TO_DATE = datetime.datetime(2010, 06, 30)
+    SITE = Wiki("", 0.5)
 #    TEMPLATE_ENNDPOINT = "/static/pages/blog.html"
 #    DATA_ENNDPOINT = "/blog/entries"
 #    PRERENDERED_ENNDPOINT = "/blog/traditional"
@@ -185,7 +185,7 @@ if BLOG_TEST:
     write_test_files(dirname, "test_freq_0.25_per_day", 20, 0.0, 0.25, "days", 1)
 
 if WIKI_TEST:
-    write_test_files(dirname, "test_freq_6_per_day", 100, 0.5, 6, "days", 0)
+    write_test_files(dirname, "test_freq_6_per_day", 100, 0.0, 1, "days", 0)
         
 # if WIKI_TEST:
     
