fc37c30c12c2991d0cf23bf1c5c0378363cb33d2
Edward Benson <eob@30-51-98.dynamic.csail.mit.edu>
2010-02-01 18:52:14 -0500

    d

--- server/static/javascripts/userLoad1.js
+++ server/static/javascripts/userLoad1.js
@@ -2,9 +2,9 @@
 
 runTestWithUsers([
     [{"time":"2010-05-01 02:16:06.310213", "clicktrail":["/static/pages/blog.html"]}],
-    [{"time":"2010-05-01 02:34:45.386383", "clicktrail":["/static/pages/flying-blog.html"]}],
+    [{"time":"2010-05-01 02:34:45.386383", "clicktrail":["/static/pages/blog-flying.html"]}],
     [{"time":"2010-05-01 02:34:45.386383", "clicktrail":["/blog/traditional"]}],
     [{"time":"2010-04-30 06:24:39.280661", "clicktrail":["/static/pages/wiki.html?pageid=1080"]}],
-    [{"time":"2010-04-30 06:24:39.280661", "clicktrail":["/static/pages/flying-wiki.html?pageid=1080"]}],
+    [{"time":"2010-04-30 06:24:39.280661", "clicktrail":["/static/pages/wiki-flying.html?pageid=1080"]}],
     [{"time":"2010-04-30 06:24:39.280661", "clicktrail":["/wiki/traditional?pageid=1080"]}]
 ]);
