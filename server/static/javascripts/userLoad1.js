c9b979966819c6dd436273d767b4a75eab5c7230
Edward Benson <eob@30-51-98.dynamic.csail.mit.edu>
2010-02-01 11:00:16 -0500

    hardy har har

--- server/static/javascripts/userLoad1.js
+++ server/static/javascripts/userLoad1.js
@@ -1,3 +1,3 @@
 // # Test Name: browser_test_freq_5_per_day  # Number Users: 20  # Percent New: 0.0   # Number Visits: 5 / days
 
-runTestWithUsers([[{"time":"2010-05-01 02:16:06.310213", "clicktrail":["/static/pages/blog.html"]}]]);
+runTestWithUsers([[{"time":"2010-05-01 02:16:06.310213", "clicktrail":["/static/pages/blog.html"]}]]);s
